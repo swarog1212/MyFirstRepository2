edf925fac83ecab0b7bf90593c771345c964a81d
george <dark8609@gmail.com>
2021-10-04 11:54:56 +0300

    4.1. Условные выражения и конструкции. Задание 4.1.18

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,23 +7,28 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             var color = Console.ReadLine();
-            if (color == "red")
+            switch (color)
             {
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.Black;
-                Console.WriteLine("Ваш цвет красный!");
-            }
-            else if (color == "green")
-            {
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.ForegroundColor = ConsoleColor.Black;
-                Console.WriteLine("Ваш чвет зеленый!");
-            }
-            else
-            {
-                Console.BackgroundColor = ConsoleColor.Cyan;
-                Console.ForegroundColor = ConsoleColor.Black;
-                Console.WriteLine("Ваш цвет бирюзовай!");
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Ваш цвет красный!");
+                break;
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Ваш чвет зеленый!");
+                break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Ваш цвет бирюзовай!");
+                break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ваш цвет желтый!");
+                break;
             }
         }
     }
