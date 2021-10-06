e2280ad86a305176cc055430ac130005726109fd
george <dark8609@gmail.com>
2021-10-06 16:28:13 +0300

    Задание 4.3.15, 4.3.16

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,60 +6,35 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //string myName = Console.ReadLine();
-
-            /*foreach (var i in myName)
+            // Задание 4.3.14
+            int[][] array = new int[3][];
+            array[0] = new int[2] { 1, 2 };
+            array[1] = new int[3] { 1, 2, 3 };
+            array[2] = new int[5] { 1, 2, 3, 4, 5 };
+            foreach (var item in array)
             {
-                Console.Write("{0} ", i);
-               
-            }
-            Console.WriteLine("Последняя буква вашего имени = {0}", myName[myName.Length - 1]);*/
-            //for (int i = myName.Length - 1; i >= 0; i--)
-            //{
-            //    Console.WriteLine(myName[i]);
-            //}
-            /*int[,] array = { { 1, 2, 3 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 } };
-            for (int i = 0; i < array.GetUpperBound(1) + 1; i++)
-            {
-                for (int k = 0; k < array.GetUpperBound(0) + 1; k++)
+                foreach (var itemArray in item)
                 {
-                    Console.Write(array[k, i] + " ");
-                }
-                Console.WriteLine();
-            }*/
-            /*var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
-            int temp = 0;
-            for (int i = 0; i < arr.Length; i++)
-            {
-                for (int k = 0; k < arr.Length - 1; k++)
-                {
-                    if (arr[k] > arr[k + 1])
-                    {
-                        temp = arr[k];
-                        arr[k] = arr[k + 1];
-                        arr[k + 1] = temp;
-                    }
+                    Console.Write(itemArray + " ");
                 }
             }
+            // Задание 4.3.15
+            int[] arr = new int[] { 5, -6, 3, 89, -34, -46, 8, -87, 45 };
+            int numbers = 0;
+            Console.WriteLine("Массив из 8-и элементов:");
             foreach (var item in arr)
             {
-                Console.Write(item);
+                Console.Write(item + " ");
             }
-            Console.WriteLine();*/
-            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
-            int sum = 0;
-            for (int i = 0; i < arr.Length; i++)
-            {
-                sum += arr[i];
-            }
-            Console.WriteLine(sum);
-            sum = 0;
-            
+            Console.WriteLine();
             foreach (var item in arr)
             {
-                sum += item;
+                if (item > 0)
+                {
+                    numbers++;
+                }
             }
-            Console.WriteLine(sum);
+            Console.WriteLine("Количество положительных чисел = " + numbers);
         }
     }
 }
